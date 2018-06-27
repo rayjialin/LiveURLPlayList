ba80f3044789f39001a2fa094f683ebd4eeebcd2
rayjialin <ruijialin@gmail.com>
2018-06-27 14:13:02 -0400

    removed dead live URLlinks

--- AVPlayerDemo/Controller/VideoViewController.swift
+++ AVPlayerDemo/Controller/VideoViewController.swift
@@ -19,26 +19,14 @@ class VideoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let urlString = "http://b028.wpc.azureedge.net/80B028/Samples/a38e6323-95e9-4f1f-9b38-75eba91704e4/5f2ce531-d508-49fb-8152-647eba422aec.ism/Manifest(format=m3u8-aapl)"
-//        guard let urlString = urlString else {return}
-//        if let url = NSURL(string: urlString){
-//            playerView = AVPlayer(url: url as URL)
-//            playerViewController.player = playerView
-//            self.present(playerViewController,animated: true){
-//                self.playerViewController.player?.play()
-//            }
-//        }
+        guard let urlString = urlString else {return}
+        if let url = NSURL(string: urlString){
+            playerView = AVPlayer(url: url as URL)
+            playerViewController.player = playerView
+            self.present(playerViewController,animated: true){
+                self.playerViewController.player?.play()
+            }
+        }
 
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-                let urlString = "http://b028.wpc.azureedge.net/80B028/Samples/a38e6323-95e9-4f1f-9b38-75eba91704e4/5f2ce531-d508-49fb-8152-647eba422aec.ism/Manifest(format=m3u8-aapl)"
-                if let url = NSURL(string: urlString){
-                    playerView = AVPlayer(url: url as URL)
-                    playerViewController.player = playerView
-                    self.present(playerViewController,animated: true){
-                        self.playerViewController.player?.play()
-                    }
-                }
     }
 }
--- AVPlayerDemo/Model/LiveURLList.swift
+++ AVPlayerDemo/Model/LiveURLList.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-let urlList = ["http://b028.wpc.azureedge.net/80B028/Samples/       a38e6323-95e9-4f1f-9b38-75eba91704e4/5f2ce531-d508-49fb-8152-647eba422aec.ism/  Manifest(format=m3u8-aapl)",
-               "http://esioslive6-i.akamaihd.net/hls/live/202892/AL_P_ESP1_FR_FRA/playlist.m3u8",
+let urlList = [
                "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
                "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
                "http://nasatv-lh.akamaihd.net/i/NASA_101@319270/index_1000_av-p.m3u8?sd=10&rebase=on",
                "http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8",
                "http://walterebert.com/playground/video/hls/sintel-trailer.m3u8",
                "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
-               "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"]
+               "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"
+              ]
